d6b16096e92c8e0b17e97b7a96385050d6e08a33
Askeladd-lucius <karlsefnithorfinn84@gmail.com>
2024-09-18 02:59:33 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,8 +12,8 @@ global.location = "Port Harcourt, Nigeria" //ur location
 
 //new
 global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
-global.ownername = 'ELITE-PRO-V2' //ur owner name
+global.ownernumber = '237650253977' //ur owner number
+global.ownername = '༒⫷ஜℐ𝓞𝓚𝓔ℛ♛𝓐𝓝𝓞𝓝𝓨𝓜𝓞𝓤𝓢⫸༒' //ur owner name
 global.websitex = "https://youtu.be/@elite-pro-empire"
 global.wagc = "https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g"
 global.themeemoji = '👨🏼‍💻'
@@ -21,9 +21,9 @@ global.wm = "ELITE-PRO-V2"
 global.botscript = 'https://github.com/Elite-MD/ELITE-PRO-V2' //script link
 global.packname = "Sticker By"
 global.author = "ELITE-PRO-V2"
-global.creator = "2348109263390@s.whatsapp.net"
-global.xprefix = '.'
-global.premium = ["2348109263390"] // Premium User
+global.creator = "237650253977@s.whatsapp.net"
+global.xprefix = '+'
+global.premium = ["237650253977"] // Premium User
 global.hituet = 0
 
 //bot sett
